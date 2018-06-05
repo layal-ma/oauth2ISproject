3fd40c5660de84a601d4c5d20ab92a2dd9dbf968
layal <layal.makhlof.93@gmail.com>
2018-06-05 08:44:27 -0700

    Add files via upload

---
+++ resource.php
@@ -0,0 +1,17 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+// include our OAuth2 Server object
+require_once __DIR__.'/server.php';
+
+// Handle a request to a resource and authenticate the access token
+if (!$server->verifyResourceRequest(OAuth2\Request::createFromGlobals())) {
+    $server->getResponse()->send();
+    die;
+}
+echo json_encode(array('success' => true, 'message' => 'You accessed THe API ! your token is valid !'));
